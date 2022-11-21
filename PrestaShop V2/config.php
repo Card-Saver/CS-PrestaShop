72b36d3547e3ec56270f88db469e376d899af2e8
Card Saver <117294895+Card-Saver@users.noreply.github.com>
2022-11-21 09:28:02 +0000

    Add files via upload

---
+++ PrestaShop V2/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'gateway_title' => 'Card Saver',
+    'method_description' => 'Pay securely via Credit / Debit Card with Card Saver',
+    'admin_module_settings_title' => 'Card Saver Settings',
+    'author' => 'PaymentNetwork',
+    'default_merchant_id' => 'CARDSAVERMMID',
+    'default_secret' => 'Sig Key',
+    'redirect_notice' => 'You will be redirected to the Card Saver gateway for payment',
+    'formfill_notice' => 'Please fill in your payment details',
+    'logo' => '/logo.gif',
+];
